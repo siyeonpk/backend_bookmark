457dea120d10ea1684dfe4c5c4c142b74d009897
siyeonpk <siyeonpk@gmail.com>
2025-02-20 12:44:52 +0900

    commit

--- src/main/java/com/bookmarker/api/controller/BookmarkController.java
+++ src/main/java/com/bookmarker/api/controller/BookmarkController.java
@@ -1,13 +1,14 @@
 package com.bookmarker.api.controller;
 
 import com.bookmarker.api.domain.Bookmark;
+import com.bookmarker.api.dto.BookmarkDTO;
 import com.bookmarker.api.dto.BookmarksDTO;
+import com.bookmarker.api.dto.CreateBookmarkRequest;
 import com.bookmarker.api.service.BookmarkService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,4 +30,12 @@ public class BookmarkController {
 
         //return bookmarkService.getBookmarks(page);
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)//201
+    //@Valid는 Data Binding을 체크하는 validator를 호출하는 역할
+    public BookmarkDTO createBookmark(@RequestBody @Valid CreateBookmarkRequest request) {
+        return bookmarkService.createBookmark(request);
+    }
+
 }
--- src/main/java/com/bookmarker/api/domain/BookmarkRepository.java
+++ src/main/java/com/bookmarker/api/domain/BookmarkRepository.java
@@ -1,6 +1,7 @@
 package com.bookmarker.api.domain;
 
 import com.bookmarker.api.dto.BookmarkDTO;
+import com.bookmarker.api.dto.BookmarkVM;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,16 +10,16 @@ import org.springframework.data.jpa.repository.Query;
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
     //class based JPA projection
-//    @Query("""
-//    select new BookmarkDTO(b.id, b.title, b.url, b.createdAt) from Bookmark b
-//    """)
     @Query("""
-    select new com.bookmarker.api.dto.BookmarkDTO(b.id, b.title, b.url, b.createdAt) from Bookmark b
+    select new BookmarkDTO(b.id, b.title, b.url, b.createdAt) from Bookmark b
     """)
+//    @Query("""
+//    select new com.bookmarker.api.dto.BookmarkDTO(b.id, b.title, b.url, b.createdAt) from Bookmark b
+//    """)
     Page<BookmarkDTO> findBookmarks(Pageable pageable);
 
     @Query("""
-    select new com.bookmarker.api.dto.BookmarkDTO(b.id, b.title, b.url, b.createdAt) from Bookmark b
+    select new BookmarkDTO(b.id, b.title, b.url, b.createdAt) from Bookmark b
     where lower(b.title) like lower(concat('%', :query, '%'))
     """)
     Page<BookmarkDTO> searchBookmarks(String query, Pageable pageable);
@@ -26,4 +27,6 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
     //method명의 naming 규칙을 보고 query문을 자동으로 생성해주는 query method
     Page<BookmarkDTO> findByTitleContainsIgnoreCase(String query, Pageable pageable);
+
+    Page<BookmarkVM> findByTitleContainingIgnoreCase(String query, Pageable pageable);
 }
